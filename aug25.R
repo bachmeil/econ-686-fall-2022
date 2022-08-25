ebf2b646005d041f32da6a99d89b6ccf3e8d8ffc
Lance Bachmeier <bachmeil@yahoo.com>
2022-08-25 14:52:54 -0500

    Create aug25.R

---
+++ aug25.R
@@ -0,0 +1,24 @@
+data.raw <- read.csv("inflation.csv", header=TRUE)
+data.raw
+dim(data.raw)
+class(data.raw)
+names(data.raw)
+names(data.raw) <- c("date", "dcpi")
+
+# First five observations of inflation
+data.raw[1:5, "dcpi"]
+data.raw[,"dcpi"]
+
+# Create variable with time series properties of inflation
+# Starts in Jan 1948, monthly data
+dcpi <- ts(data.raw[,"dcpi"], start=c(1948,1), frequency=12)
+# Don't do this
+dcpi <- ts(data.raw[,"dcpi"], start=c(1948,1), end=c(2022,7))
+dcpi
+
+mean(dcpi)
+plot(dcpi)
+# window -> select part of the sample
+mean(window(dcpi, start=c(1990,1)))
+mean(window(dcpi, start=c(1990,1), end=c(2019,12)))
+plot(window(dcpi, start=c(2000,1), end=c(2014,12)))
